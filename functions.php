76c9b394dfac31ed17732c4e11ccb1917cec4fb4
aadamhashmi1 <muhammadaadamhashmi@gmail.com>
2025-08-16 15:54:29 +0500

    functions

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@ function child_enqueue_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'child_enqueue_styles');
+function child_register_menus() {
+    register_nav_menus([
+        'secondary' => __('Secondary Menu', 'astra-child'),
+    ]);
+}
+add_action('after_setup_theme', 'child_register_menus');
